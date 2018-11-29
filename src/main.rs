f850dcdebbb9080fb03c65e4a48b75d931081999
Gareth <gareth.w.owen@gmail.com>
2018-11-29 21:24:33 +0000

    Added macros

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
 extern crate glfw;
 use self::glfw::{Context, Key, Action};
 
@@ -7,20 +9,22 @@ use self::gl::types::*;
 // Huh, so mod is sorta like #include...
 mod common;
 mod shader;
+mod macros;
 
 use std::ptr;
 use std::mem;
 use std::os::raw::c_void;
-use std::ffi::CStr;
 use std::path::Path;
+use std::ffi::{CString, CStr};
 
 use shader::Shader;
 
 extern crate image;
-use image::GenericImage;
+use image::GenericImageView;
+use image::DynamicImage::*;
 
 extern crate cgmath;
-use cgmath::{Matrix4, Vector3, vec3, Deg, perspective, Point3};
+use cgmath::{Matrix4, Vector3, vec3, Deg, Rad, perspective, Point3};
 use cgmath::prelude::*;
 
 // settings 
@@ -32,7 +36,7 @@ fn main() {
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
     glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
 
-    let (mut window, events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "Cube", glfw::WindowMode::Windowed)
+    let (mut window, events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "Rust rotating cube!", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
     
     let mut deltaTime: f32;
@@ -43,26 +47,181 @@ fn main() {
 
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
-    // let (ourShader, VBO, VAO, texture1, texture2) = unsafe {
-    //     gl::Enable(gl::DEPTH_TEST);
+    let (ourShader, VBO, VAO, texture1, texture2) = unsafe {
+        gl::Enable(gl::DEPTH_TEST);
 
-    //     let ourShader = Shader::new(
-    //         "src/vertshader.glsl",
-    //         "src/fragshader.glsl"
-    //     );
+        let ourShader = Shader::new(
+            "../../src/vertshader.glsl",
+            "../../src/fragshader.glsl"
+        );
 
-    // };
+        let vertices: [f32; 180] = [
+             -0.5, -0.5, -0.5,  0.0, 0.0,
+              0.5, -0.5, -0.5,  1.0, 0.0,
+              0.5,  0.5, -0.5,  1.0, 1.0,
+              0.5,  0.5, -0.5,  1.0, 1.0,
+             -0.5,  0.5, -0.5,  0.0, 1.0,
+             -0.5, -0.5, -0.5,  0.0, 0.0,
+
+             -0.5, -0.5,  0.5,  0.0, 0.0,
+              0.5, -0.5,  0.5,  1.0, 0.0,
+              0.5,  0.5,  0.5,  1.0, 1.0,
+              0.5,  0.5,  0.5,  1.0, 1.0,
+             -0.5,  0.5,  0.5,  0.0, 1.0,
+             -0.5, -0.5,  0.5,  0.0, 0.0,
+
+             -0.5,  0.5,  0.5,  1.0, 0.0,
+             -0.5,  0.5, -0.5,  1.0, 1.0,
+             -0.5, -0.5, -0.5,  0.0, 1.0,
+             -0.5, -0.5, -0.5,  0.0, 1.0,
+             -0.5, -0.5,  0.5,  0.0, 0.0,
+             -0.5,  0.5,  0.5,  1.0, 0.0,
+
+              0.5,  0.5,  0.5,  1.0, 0.0,
+              0.5,  0.5, -0.5,  1.0, 1.0,
+              0.5, -0.5, -0.5,  0.0, 1.0,
+              0.5, -0.5, -0.5,  0.0, 1.0,
+              0.5, -0.5,  0.5,  0.0, 0.0,
+              0.5,  0.5,  0.5,  1.0, 0.0,
+
+             -0.5, -0.5, -0.5,  0.0, 1.0,
+              0.5, -0.5, -0.5,  1.0, 1.0,
+              0.5, -0.5,  0.5,  1.0, 0.0,
+              0.5, -0.5,  0.5,  1.0, 0.0,
+             -0.5, -0.5,  0.5,  0.0, 0.0,
+             -0.5, -0.5, -0.5,  0.0, 1.0,
+
+             -0.5,  0.5, -0.5,  0.0, 1.0,
+              0.5,  0.5, -0.5,  1.0, 1.0,
+              0.5,  0.5,  0.5,  1.0, 0.0,
+              0.5,  0.5,  0.5,  1.0, 0.0,
+             -0.5,  0.5,  0.5,  0.0, 0.0,
+             -0.5,  0.5, -0.5,  0.0, 1.0
+        ];
+
+        let (mut VBO, mut VAO) = (0, 0);
+        gl::GenVertexArrays(1, &mut VAO);
+        gl::GenBuffers(1, &mut VBO);
+
+        gl::BindVertexArray(VAO);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
+        gl::BufferData(gl::ARRAY_BUFFER,
+            (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            &vertices[0] as *const f32 as *const c_void,
+            gl::STATIC_DRAW);
+        
+        let stride = 5 * mem::size_of::<GLfloat>() as GLsizei;
+        // position attribute
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
+        gl::EnableVertexAttribArray(0);
+        // texture coord attribute
+        gl::VertexAttribPointer(1, 2, gl::FLOAT, gl::FALSE, stride, (3 * mem::size_of::<GLfloat>()) as *const c_void);
+        gl::EnableVertexAttribArray(1);
+
+        // load and create a texture
+        let (mut texture1, mut texture2) = (0, 0);
+
+        // texture 1
+        gl::GenTextures(1, &mut texture1);
+        //texture wrapping parameters
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+        // set texture filtering parameters
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+        // load image, create texture and generate mipmaps
+        let img = image::open(&Path::new("../../resources/textures/container.jpg")).expect("Failed to load texture");
+        let data = img.raw_pixels();
+        gl::TexImage2D(gl::TEXTURE_2D,
+                       0,
+                       gl::RGB as i32,
+                       img.width() as i32,
+                       img.height() as i32,
+                       0, 
+                       gl::RGB,
+                       gl::UNSIGNED_BYTE,
+                       &data[0] as *const u8 as *const c_void);
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+        // texture 2
+        gl::GenTextures(1, &mut texture2);
+        //texture wrapping parameters
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+        // set texture filtering parameters
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+        // load image, create texture and generate mipmaps
+        let img = image::open(&Path::new("../../resources/textures/awesomeface.png")).expect("Failed to load texture");
+        let img = img.flipv();
+        let data = img.raw_pixels();
+        gl::TexImage2D(gl::TEXTURE_2D,
+                       0,
+                       gl::RGB as i32,
+                       img.width() as i32,
+                       img.height() as i32,
+                       0, 
+                       gl::RGB,
+                       gl::UNSIGNED_BYTE,
+                       &data[0] as *const u8 as *const c_void);
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+
+        ourShader.useProgram();
+        ourShader.setInt(c_str!("texture1"), 0);
+        ourShader.setInt(c_str!("texture2"), 0);
+
+        (ourShader, VBO, VAO, texture1, texture2)
+    };
 
     while !window.should_close() {
-        glfw.poll_events();
+
+        let currentFrame = glfw.get_time() as f32;
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        
 
         unsafe {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+
+            gl::ActiveTexture(gl::TEXTURE0);
+            gl::BindTexture(gl::TEXTURE_2D, texture1);
+            gl::ActiveTexture(gl::TEXTURE1);
+            gl::BindTexture(gl::TEXTURE_2D, texture2);
+
+            ourShader.useProgram();
+
+            // create transformations
+            // also, the equivalent of glm::rotate requires a normalized vector. Bit different
+            // from what I'm used to but it will adapt to service us :)
+            let model: Matrix4<f32>= Matrix4::from_axis_angle(vec3(0.5, 1.0, 0.0).normalize(), Rad(currentFrame));
+            let view: Matrix4<f32> = Matrix4::from_translation(vec3(0.0, 0.0, -3.0));
+            let projection: Matrix4<f32> = perspective(Rad(45.0), SCR_WIDTH as f32 / SCR_HEIGHT as f32, 0.1, 100.0);
+            // matrix uniform locations
+            let modelLoc = gl::GetUniformLocation(ourShader.ID, c_str!("model").as_ptr());
+            let viewLoc = gl::GetUniformLocation(ourShader.ID, c_str!("view").as_ptr());
+            // pass them to the shaders
+            gl::UniformMatrix4fv(modelLoc, 1, gl::FALSE, model.as_ptr());
+            gl::UniformMatrix4fv(viewLoc, 1, gl::FALSE, &view[0][0]);
+            // set projection matrix, probably better off outside but eh I'm just messing
+            // not trying to write efficiently
+            ourShader.setMat4(c_str!("projection"), &projection);
+
+            // render
+            gl::BindVertexArray(VAO);
+            gl::DrawArrays(gl::TRIANGLES, 0, 36);
+
         };
-        
+        window.swap_buffers();
+        glfw.poll_events();
+
         for (_, event) in glfw::flush_messages(&events) {
             handle_window_event(&mut window, event);
+        }
+
+        unsafe {
+            gl::DeleteVertexArrays(1, &VAO);
+            gl::DeleteBuffers(1, &VBO);
         }
     }
 }
